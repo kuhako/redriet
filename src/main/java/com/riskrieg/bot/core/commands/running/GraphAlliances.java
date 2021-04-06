7dfb4ff1116798210ec52c18a6d2f595678b2dc6
Aaron Yoder <aaronjyoder@gmail.com>
2021-04-05 21:38:51 -0400

    Fix potential issue with getting allied players

--- src/main/java/com/riskrieg/bot/core/commands/running/GraphAlliances.java
+++ src/main/java/com/riskrieg/bot/core/commands/running/GraphAlliances.java
@@ -1,31 +1,25 @@
 package com.riskrieg.bot.core.commands.running;
 
-import com.mxgraph.layout.*;
+import com.mxgraph.layout.mxCircleLayout;
 import com.mxgraph.util.mxCellRenderer;
 import com.mxgraph.util.mxConstants;
 import com.mxgraph.view.mxGraph;
 import com.riskrieg.api.Riskrieg;
-import com.riskrieg.bot.constant.BotConstants;
 import com.riskrieg.bot.core.Command;
 import com.riskrieg.bot.core.input.MessageInput;
 import com.riskrieg.bot.core.input.SlashInput;
+import com.riskrieg.bot.util.Error;
 import com.riskrieg.bot.util.ImageUtil;
 import com.riskrieg.constant.Colors;
-import com.riskrieg.bot.util.Error;
 import com.riskrieg.gamemode.Game;
 import com.riskrieg.gamemode.IAlliances;
 import com.riskrieg.nation.AllianceNation;
 import com.riskrieg.nation.Nation;
-import org.jgrapht.Graph;
-import org.jgrapht.ext.*;
-import org.jgrapht.graph.DefaultEdge;
-import org.jgrapht.graph.SimpleGraph;
 import java.awt.image.BufferedImage;
-import java.awt.Color;
-import java.util.Set;
-import java.util.stream.Collectors;
 import java.util.HashMap;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.utils.AttachmentOption;
 
@@ -34,7 +28,7 @@ public class GraphAlliances extends Command {
   public GraphAlliances() {
     this.settings.setAliases("alliesgraph", "graphallies");
     this.settings.setDescription("Lists all alliances in the game in the form of a graph.");
-    this.settings.setEmbedColor(BotConstants.GENERIC_CMD_COLOR);
+    this.settings.setEmbedColor(Colors.BORDER_COLOR);
     this.settings.setGuildOnly(true);
   }
 
@@ -58,7 +52,7 @@ public class GraphAlliances extends Command {
     layout.setDisableEdgeStyle(false);
     layout.execute(graph.getDefaultParent());
 
-    BufferedImage image = mxCellRenderer.createBufferedImage(graph, null, 2, Colors.TERRITORY_COLOR , true, null);
+    BufferedImage image = mxCellRenderer.createBufferedImage(graph, null, 2, Colors.TERRITORY_COLOR, true, null);
     String filename = "ally-graph";
 
     EmbedBuilder embedBuilder = new EmbedBuilder();
@@ -71,26 +65,27 @@ public class GraphAlliances extends Command {
   private mxGraph generateAllyGraph(Game game) {
     Set<Nation> nationList = game.getNations();
     mxGraph graph = new mxGraph();
-    
+
     String borderColour = String.format("%06x", 0xFFFFFF & Colors.BORDER_COLOR.getRGB());
-    HashMap<String, Object> vertices = new HashMap<String, Object>();
+    HashMap<String, Object> vertices = new HashMap<>();
 
     //Create the graph by adding the player names as the vertices and then associating them
-    for (Nation nation: nationList) {
+    for (Nation nation : nationList) {
       String name = game.getPlayer(nation.getLeaderIdentifier().id()).get().getName();
       String colour = String.format("%06x", 0xFFFFFF & nation.getLeaderIdentifier().color().value().getRGB());
       vertices.put(name, graph.insertVertex(
-        graph.getDefaultParent(), name, null, 0, 0, 15, 15,
-        "ellipse;whiteSpace=wrap;html=1;aspect=fixed;strokeWidth=1;noLabel=1;fillColor=#" + colour + ";strokeColor=#" + borderColour)
+          graph.getDefaultParent(), name, null, 0, 0, 15, 15,
+          "ellipse;whiteSpace=wrap;html=1;aspect=fixed;strokeWidth=1;noLabel=1;fillColor=#" + colour + ";strokeColor=#" + borderColour)
       );
-    } for (Nation nation: nationList) {
+    }
+    for (Nation nation : nationList) {
       String colour = String.format("%06x", 0xFFFFFF & nation.getLeaderIdentifier().color().value().getRGB());
 
       Set<Nation> allies = ((AllianceNation) nation).getAllies().stream()
-        .map(ally -> game.getNation(ally.id()).get())
-        .collect(Collectors.toSet()
-      );
-      for (Nation ally: allies) {
+          .filter(ally -> game.getNation(ally.id()).isPresent())
+          .map(ally -> game.getNation(ally.id()).get())
+          .collect(Collectors.toSet());
+      for (Nation ally : allies) {
         String name = game.getPlayer(nation.getLeaderIdentifier().id()).get().getName();
         String nameAlly = game.getPlayer(ally.getLeaderIdentifier().id()).get().getName();
         String style = "endArrow=classic;startArrow=none;strokeWidth=1;curved=1;snapToPoint=1;dashed=1;dashPattern=1;noLabel=1;strokeColor=#" + colour;
@@ -107,7 +102,7 @@ public class GraphAlliances extends Command {
     //If labels in vertices are to be set back on, uncomment this and set noLabel to 0 in style vertices and also increase the width and height
     /*vertexStyle.put(mxConstants.STYLE_LABEL_BACKGROUNDCOLOR, "#e0dad9");
     mxConstants.STYLE_ALIGN = mxConstants.ALIGN_CENTER;
-    mxConstants.LABEL_INSET=1;*/ 
+    mxConstants.LABEL_INSET=1;*/
 
     var edgeStyle = graph.getStylesheet().getDefaultEdgeStyle();
     edgeStyle.put(mxConstants.STYLE_EDGE, mxConstants.SHAPE_LINE);
@@ -120,22 +115,23 @@ public class GraphAlliances extends Command {
   }
 
   private Boolean hasAnyError(Optional<Game> optGame, MessageInput input) {
-    if (!optGame.isPresent()) {
+    if (optGame.isEmpty()) {
       input.event().getChannel().sendMessage(Error.create("You need to create a game before using this command.", this.settings)).queue();
       return true;
     } else if (optGame.get().getPlayers().size() != optGame.get().getNations().size()) {
       input.event().getChannel().sendMessage(Error.create("Not all players have selected a capital.", this.settings)).queue();
       return true;
-    } else if (optGame.get().getNations().size() == 0 && optGame.get().getPlayers().size() == 0) {
+    } else if (optGame.get().getPlayers().size() == 0 && optGame.get().getNations().size() == 0) {
       input.event().getChannel().sendMessage(Error.create("Nobody has joined the game yet.", this.settings)).queue();
       return true;
     } else if (!(optGame.get() instanceof IAlliances)) {
       input.event().getChannel().sendMessage(Error.create("Invalid game mode.", this.settings)).queue();
       return true;
-    } else if (!optGame.get().getGameRule("alliances").isPresent() && !optGame.get().getGameRule("alliances").get().isEnabled()) {
+    } else if (optGame.get().getGameRule("alliances").isEmpty() && !optGame.get().getGameRule("alliances").get().isEnabled()) {
       input.event().getChannel().sendMessage(Error.create("The alliances game rule is disabled.", this.settings)).queue();
       return true;
     }
     return false;
   }
+
 }
